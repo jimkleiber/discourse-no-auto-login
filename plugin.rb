57f80da1699a16012a2a515fc837195a5a4c251a
Jim Kleiber <hello@jimkleiber.com>
2022-03-16 01:25:58 -0400

    Added site setting to enable.

--- plugin.rb
+++ plugin.rb
@@ -4,12 +4,16 @@
 # authors: Jim Kleiber
 # url: https://github.com/jimkleiber/discourse-no-auto-login
 
+enabled_site_setting :no_auto_login_enabled
+
 class ::ApplicationController
   module NoAutoLoginRedirectToLogin
 		def redirect_to_login
-			dont_cache_page
-			cookies[:destination_url] = destination_url
-			redirect_to path("/login")
+			if SiteSetting.awesomeness_enabled
+				dont_cache_page
+				cookies[:destination_url] = destination_url
+				redirect_to path("/login")
+			end
 		end
   end
   singleton_class.prepend NoAutoLoginRedirectToLogin
