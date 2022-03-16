1981a6cc8b48110cae62946168eceadeb0605002
Jim Kleiber <hello@jimkleiber.com>
2022-03-16 03:41:41 -0400

    Moved conditional to encompass the class

--- plugin.rb
+++ plugin.rb
@@ -7,16 +7,16 @@
 enabled_site_setting :no_auto_login_enabled
 
 after_initialize do
-	class ::ApplicationController
-	  module NoAutoLoginRedirectToLogin
-		def redirect_to_login
- 			if SiteSetting.no_auto_login_enabled
+ 	if SiteSetting.no_auto_login_enabled	
+		class ::ApplicationController
+		  module NoAutoLoginRedirectToLogin
+			def redirect_to_login
 				dont_cache_page
 				cookies[:destination_url] = destination_url
 				redirect_to path("/login")
- 			end
+			end
+		  end	
+		  prepend NoAutoLoginRedirectToLogin
 		end
-	  end
-	  prepend NoAutoLoginRedirectToLogin
 	end
 end
